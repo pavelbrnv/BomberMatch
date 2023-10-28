3e2f62b2e3140b510055fa51be6e0fbe8237d680
Baranov Pavel <@>
2023-10-28 19:35:05 +0300

    Adapter for python bombers. Cobra implementation.

---
+++ BomberMatch/Bombers/Python/PythonBomber.cs
@@ -0,0 +1,54 @@
+﻿namespace BomberMatch.Bombers.Python
+{
+	public sealed class PythonBomber : IBomber
+	{
+		private readonly dynamic getName;
+		private readonly dynamic setRules;
+		private readonly dynamic go;
+
+		public PythonBomber(string scriptName)
+		{			
+			var engine = IronPython.Hosting.Python.CreateEngine();
+			var scope = engine.CreateScope();
+			engine.ExecuteFile(scriptName, scope);
+			var bomberType = scope.GetVariable("Bomber");
+			var bomber = engine.Operations.CreateInstance(bomberType);
+
+			getName = engine.Operations.GetMember<Func<string>>(bomber, "get_name");
+			setRules = engine.Operations.GetMember<Action<int, int, int>>(bomber, "set_rules");
+			go = engine.Operations.GetMember<Func<int[][], int[][], int[], int>>(bomber, "go");
+		}
+
+		public string Name => getName();
+		
+		public void SetRules(int matchActionsNumber, int detonationRadius, int timeToDetonate)
+		{
+			setRules(matchActionsNumber, detonationRadius, timeToDetonate);
+		}
+
+		public int Go(int[,] arena, int[,] bombers, int[] availableMoves)
+		{
+			return go(ConvertArray(arena), ConvertArray(bombers), availableMoves);
+		}
+
+		private static int[][] ConvertArray(int[,] items)
+		{
+			var rows = items.GetLength(0);
+			var columns = items.GetLength(1);
+
+			var result = new int[rows][];
+
+			for (var i = 0; i < columns; i++)
+			{
+				var row = new int[columns];
+				for (var j = 0; j < columns; j++)
+				{
+					row[j] = items[i, j];
+				}
+				result[i] = row;
+			}
+
+			return result;
+		}
+	}
+}
---
+++ BomberMatch/DummyObserver.cs
@@ -0,0 +1,17 @@
+﻿namespace BomberMatch
+{
+	public sealed class DummyObserver : IMatchObserver
+	{
+		public void StartMatch(uint bombDetonationRadius, uint bombTimeToDetonate, Arena arena)
+		{
+		}
+
+		public void AddRound(MatchRound round)
+		{
+		}
+
+		public void EndMatch()
+		{
+		}		
+	}
+}
